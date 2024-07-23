f3edebb00adc6a06d0b4f8b5b99f5e8080d12f7b
ab-ankush <ankusha062@gmail.com>
2024-07-23 12:42:09 +0530

    back button added

---
+++ src/components/BackButton.jsx
@@ -0,0 +1,58 @@
+import React from "react";
+
+const BackButton = () => {
+  return (
+    <div className="w-full">
+      <button onClick={() => window.history.back()} className="w-full">
+        {/* <svg
+          fill="#000000"
+          height="30px"
+          width="30px"
+          version="1.1"
+          id="Layer_1"
+          viewBox="0 0 330 330"
+        >
+          <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+          <g
+            id="SVGRepo_tracerCarrier"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+          ></g>
+          <g id="SVGRepo_iconCarrier">
+            {" "}
+            <path
+              id="XMLID_6_"
+              d="M165,0C74.019,0,0,74.019,0,165s74.019,165,165,165s165-74.019,165-165S255.981,0,165,0z M205.606,234.394 c5.858,5.857,5.858,15.355,0,21.213C202.678,258.535,198.839,260,195,260s-7.678-1.464-10.606-4.394l-80-79.998 c-2.813-2.813-4.394-6.628-4.394-10.606c0-3.978,1.58-7.794,4.394-10.607l80-80.002c5.857-5.858,15.355-5.858,21.213,0 c5.858,5.857,5.858,15.355,0,21.213l-69.393,69.396L205.606,234.394z"
+            ></path>{" "}
+          </g>
+        </svg> */}
+        <svg
+          fill="#fff"
+          version="1.1"
+          id="Capa_1"
+          width="30px"
+          height="30px"
+          viewBox="0 0 400.004 400.004"
+          stroke="#000000"
+        >
+          <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+          <g
+            id="SVGRepo_tracerCarrier"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+          ></g>
+          <g id="SVGRepo_iconCarrier">
+            {" "}
+            <g>
+              {" "}
+              <path d="M382.688,182.686H59.116l77.209-77.214c6.764-6.76,6.764-17.726,0-24.485c-6.764-6.764-17.73-6.764-24.484,0L5.073,187.757 c-6.764,6.76-6.764,17.727,0,24.485l106.768,106.775c3.381,3.383,7.812,5.072,12.242,5.072c4.43,0,8.861-1.689,12.242-5.072 c6.764-6.76,6.764-17.726,0-24.484l-77.209-77.218h323.572c9.562,0,17.316-7.753,17.316-17.315 C400.004,190.438,392.251,182.686,382.688,182.686z"></path>{" "}
+            </g>{" "}
+          </g>
+        </svg>
+        {/* Back */}
+      </button>
+    </div>
+  );
+};
+
+export default BackButton;
