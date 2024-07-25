a0eeff9b6d12bf7e5abe1ed4a65282d1768021b3
ab-ankush <ankusha062@gmail.com>
2024-07-25 13:25:40 +0530

    first two components working

--- src/components/explore/TopPickings.jsx
+++ src/components/explore/TopPickings.jsx
@@ -18,12 +18,10 @@ const numCardsToDisplay = () => {
 };
 
 const TopPickings = () => {
-  const [currentIndex, setCurrentIndex] = useState(0);
-  const [numVisibleCards, setNumVisibleCards] = useState(3);
   const [visibleCards, setVisibleCards] = useState(0);
   const [resorts, setResorts] = useState([]);
   const [loadedImages, setLoadedImages] = useState({});
-  const [isAnimatingIndex, setIsAnimatingIndex] = useState(null); // Track index for animation
+  const [isLoading, setIsLoading] = useState(true);
   const api = base_url;
   const defaultImageUrl = "/hoe-img3.jpeg";
 
@@ -34,6 +32,7 @@ const TopPickings = () => {
         const data = await res.json();
         const limitedResorts = data.slice(0, RANGE);
         setResorts(limitedResorts);
+        setIsLoading(false);
       } catch (error) {
         console.error("Failed to fetch resorts:", error);
       }
@@ -77,42 +76,6 @@ const TopPickings = () => {
     return url.replace("dl=0", "raw=1");
   };
 
-  const handlePrevClick = () => {
-    if (isAnimatingIndex !== null) return;
-    setIsAnimatingIndex(currentIndex);
-    setCurrentIndex(
-      (prevIndex) => (prevIndex - 1 + resorts.length) % resorts.length
-    );
-    setTimeout(() => {
-      setIsAnimatingIndex(null);
-    }, 500);
-  };
-
-  const handleNextClick = () => {
-    if (isAnimatingIndex !== null) return;
-    setIsAnimatingIndex(currentIndex);
-    setCurrentIndex((prevIndex) => (prevIndex + 1) % resorts.length);
-    setTimeout(() => {
-      setIsAnimatingIndex(null);
-    }, 500);
-  };
-
-  const updateNumVisibleCards = () => {
-    if (window.innerWidth < 768) {
-      setNumVisibleCards(1);
-    } else if (window.innerWidth < 1024) {
-      setNumVisibleCards(2);
-    } else {
-      setNumVisibleCards(3);
-    }
-  };
-
-  useEffect(() => {
-    updateNumVisibleCards();
-    window.addEventListener("resize", updateNumVisibleCards);
-    return () => window.removeEventListener("resize", updateNumVisibleCards);
-  }, []);
-
   const getVisibleResorts = () => {
     return resorts.slice(0, visibleCards);
   };
@@ -133,40 +96,47 @@ const TopPickings = () => {
           <p className="text-black text-sm">50+ resorts</p>
         </div>
       </div>
-      <div className="container mx-auto max-w-6xl lg:px-0 px-8">
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3  gap-4">
-          {resorts.length > 0 &&
-            getVisibleResorts().map((resort, index) => (
-              <Link href={`/resortdetail?id=${resort._id}`} key={resort._id}>
-                <div
-                  className={`rounded-lg shadow-lg overflow-hidden hover:cursor-pointer transform transition-transform duration-500 ease-in-out `}
-                  style={{
-                    backgroundImage: `url(${
-                      loadedImages[(currentIndex + index) % resorts.length]
-                        ? getDirectImageUrl(resort.photoUrls[0])
-                        : defaultImageUrl
-                    })`,
-                    backgroundSize: "cover",
-                    backgroundPosition: "center",
-                    height: "350px",
-                    opacity: index === 0 ? "100" : "50", // Adjust opacity based on index
-                  }}
-                >
-                  <div className="p-4 bottom-0 w-full mb-0 bg-black bg-opacity-50 text-white mt-[17rem] transition-opacity duration-500 ease-in-out">
-                    <h3 className="text-xl font-f_3">{resort.name}</h3>
-                    <p>
-                      {resort.city}, {resort.country}
-                    </p>
-                  </div>
-                </div>
-              </Link>
-            ))}
-        </div>
 
-        {resorts.length > 0 && visibleCards < resorts.length && (
-          <SeeMoreButton func={addTwoNewCards} />
-        )}
-      </div>
+      {isLoading ? (
+        <div className="flex justify-center items-center">
+          <div className="animate-spin rounded-full h-12 w-12 border-t-2 border-b-2 border-gray-900"></div>
+        </div>
+      ) : (
+        <div className="container mx-auto max-w-6xl lg:px-0 px-8">
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3  gap-4">
+            {resorts.length > 0 &&
+              getVisibleResorts().map((resort, index) => (
+                <Link href={`/resortdetail?id=${resort._id}`} key={resort._id}>
+                  <div
+                    className={`rounded-lg shadow-lg overflow-hidden hover:cursor-pointer transform transition-transform duration-500 ease-in-out `}
+                    style={{
+                      backgroundImage: `url(${
+                        loadedImages[index % resorts.length]
+                          ? getDirectImageUrl(resort.photoUrls[0])
+                          : defaultImageUrl
+                      })`,
+                      backgroundSize: "cover",
+                      backgroundPosition: "center",
+                      height: "350px",
+                      opacity: index === 0 ? "100" : "50", // Adjust opacity based on index
+                    }}
+                  >
+                    <div className="p-4 bottom-0 w-full mb-0 bg-black bg-opacity-50 text-white mt-[17rem] transition-opacity duration-500 ease-in-out">
+                      <h3 className="text-xl font-f_3">{resort.name}</h3>
+                      <p>
+                        {resort.city}, {resort.country}
+                      </p>
+                    </div>
+                  </div>
+                </Link>
+              ))}
+          </div>
+
+          {resorts.length > 0 && visibleCards < resorts.length && (
+            <SeeMoreButton func={addTwoNewCards} />
+          )}
+        </div>
+      )}
     </div>
   );
 };
