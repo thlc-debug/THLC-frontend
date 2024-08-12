a6c3187c5e90bb0436e47544679ca7ec06b5db0b
smalik21 <sahilmalik25007@gmail.com>
2024-08-12 22:50:48 +0530

    fix

--- src/app/luxury-chains/page.js
+++ src/app/luxury-chains/page.js
@@ -4,19 +4,19 @@ import Hero from "@/components/luxury-chains/HeroSection";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 import Chains from "@/components/luxury-chains/Chains";
-import { fetchHotels } from "@/utils/actions";
+// import { fetchHotels } from "@/utils/actions";
 
 export default async function LuxuryChainsPage() {
-  const hotels = await fetchHotels();
+  // const hotels = await fetchHotels();
 
   return (
     <main className="font-f_3">
       <Header />
-      <Hero />
+      {/* <Hero />
       <h1 className="sr-only">Luxury Chains</h1>
       <h2 className="sr-only">Find hotels as per your convenience...</h2>
       <Chains hotels={hotels} />
-      <Footer />
+      <Footer /> */}
     </main>
   );
 }
