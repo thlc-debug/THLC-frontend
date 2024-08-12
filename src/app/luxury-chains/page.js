6c47e109a117e32b1c18bdf512817ae4b323d890
smalik21 <sahilmalik25007@gmail.com>
2024-08-12 22:34:59 +0530

    fix

--- src/app/luxury-chains/page.js
+++ src/app/luxury-chains/page.js
@@ -1,32 +1,22 @@
-import Card from "@/components/luxury-chains/Card";
+// app/luxury-chains/page.jsx
+
 import Hero from "@/components/luxury-chains/HeroSection";
-import Search from "@/components/luxury-chains/Search";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 import Chains from "@/components/luxury-chains/Chains";
+import { fetchHotels } from "@/utils/actions";
 
-const Page = () => {
+export default async function LuxuryChainsPage() {
+  const hotels = await fetchHotels();
+
   return (
     <main className="font-f_3">
       <Header />
       <Hero />
       <h1 className="sr-only">Luxury Chains</h1>
       <h2 className="sr-only">Find hotels as per your convenience...</h2>
-      {/* <Search
-        searchInput={searchInput}
-        handleInput={handleInput}
-        handleFilter={applyFilter}  
-      /> */}
-      <Chains />
+      <Chains hotels={hotels} />
       <Footer />
     </main>
   );
-};
-
-// const HotelsPageWithSuspense = () => (
-//   <Suspense fallback={<Loader />}>
-//     <Page />
-//   </Suspense>
-// );
-
-export default Page;
+}
