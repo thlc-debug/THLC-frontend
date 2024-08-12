79e0060a361feb90ed53a3f2ddbdfcd657c64d0d
smalik21 <sahilmalik25007@gmail.com>
2024-08-12 23:19:23 +0530

    fix

--- src/app/luxury-chains/page.js
+++ src/app/luxury-chains/page.js
@@ -4,10 +4,10 @@ import Hero from "@/components/luxury-chains/HeroSection";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 import Chains from "@/components/luxury-chains/Chains";
-// import { fetchHotels } from "@/utils/actions";
+import { fetchHotels } from "@/utils/actions";
 
 export default async function LuxuryChainsPage() {
-  // const hotels = await fetchHotels();
+  const hotels = await fetchHotels();
 
   return (
     <main className="font-f_3">
