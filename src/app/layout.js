895230a8e7d97517e0dc2a94715c666573aa7959
smalik21 <sahilmalik25007@gmail.com>
2024-08-13 15:44:15 +0530

    g-tag

--- src/app/layout.js
+++ src/app/layout.js
@@ -18,6 +18,20 @@ export default function RootLayout({ children }) {
         <link rel="canonical" href="https://theluxuryhotelconcierge.com/" />
         <Script
           async
+          src="https://www.googletagmanager.com/gtag/js?id=G-KTWVZ8J9KX"
+        ></Script>
+        <Script id="google-tag-manager">
+          {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-KTWVZ8J9KX');
+          `}
+        </Script>
+
+        <Script
+          async
           src="https://www.googletagmanager.com/gtag/js?id=G-X0KQ7SFZ67"
         ></Script>
         <Script id="google-tag-manager">
