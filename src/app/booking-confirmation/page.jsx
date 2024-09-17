b228c877a947037e6d495f22463f1ac74911a024
Ankush Bhagat <ankusha062@gmail.com>
2024-09-17 15:03:24 +0530

    added ccavenue success and failure pages

--- src/app/booking-confirmation/page.jsx
+++ src/app/booking-confirmation/page.jsx
@@ -1,6 +1,12 @@
 "use client";
 
+import Link from "next/link";
 import { useSearchParams } from "next/navigation";
+import { useEffect, useState } from "react";
+import Lottie from "react-lottie";
+
+import Tick from "/public/lotties/tick.json";
+import Cross from "/public/lotties/cross.json";
 
 const BookingConfirmation = () => {
   const searchParams = useSearchParams();
@@ -8,27 +14,173 @@ const BookingConfirmation = () => {
   const data = Object.fromEntries(searchParams.entries());
   console.log(data);
 
-  const orderId = searchParams.get("order_id");
-  const trackingID = searchParams.get("tracking_id");
-  const paymentMethod = searchParams.get("payment_method");
-  const bankRefNo = searchParams.get("bank_ref_no");
-  const currency = searchParams.get("currency");
-  const amount = searchParams.get("amount");
-  const message = searchParams.get("status_message");
-  const transDate = searchParams.get("trans_date");
-
   return (
-    <div>
-      <h1>Booking Confirmation</h1>
-      <p>Order ID: {orderId}</p>
-      <p>Tracking ID: {trackingID}</p>
-      <p>Payment Method: {paymentMethod}</p>
-      <p>Bank Reference Number: {bankRefNo}</p>
-      <p>Currency: {currency}</p>
-      <p>Amount: {amount}</p>
-      <p>Message: {message}</p>
-      <p>Transaction Date: {transDate}</p>
-    </div>
+    <>
+      {data.order_status === "Success" ? (
+        <div className="h-screen flex justify-center items-center bg-gray-100">
+          <div className="h-[90%] w-2/3 rounded-xl bg-white">
+            <div className="p-20">
+              <div>
+                <div>
+                  <div>
+                    <div className="flex gap-2">
+                      <h1 className="text-3xl font-medium font-f_2">
+                        Your Order is Confirmed!
+                      </h1>
+                      <div className="flex justify-start">
+                        <Lottie
+                          options={{
+                            loop: true,
+                            autoplay: true,
+                            animationData: Tick,
+                            rendererSettings: {
+                              preserveAspectRatio: "xMidYMid slice",
+                            },
+                          }}
+                          height={40}
+                          width={40}
+                        />
+                      </div>
+                    </div>
+
+                    <div className="mt-12">
+                      <p className="text-gray-500">
+                        Order ID: #{data.order_id}
+                      </p>
+                      <p className="font-medium">
+                        Thank you For choosing us,{" "}
+                        {data.billing_name.split(" ")[0]}
+                      </p>
+                    </div>
+                  </div>
+
+                  <div className="p-6 mt-12 border border-1 rounded-lg">
+                    <h3 className="text-lg font-bold mb-4">Order Info</h3>
+                    <div className="grid grid-cols-2 gap-4 text-sm text-gray-600">
+                      {/* Order Date */}
+                      <div>
+                        <p className="text-gray-400">Order Date</p>
+                        <p className="font-semibold text-gray-900">
+                          {data.trans_date}
+                        </p>
+                      </div>
+
+                      {/* Delivery Date */}
+                      <div>
+                        <p className="text-gray-400">Tracking ID</p>
+                        <p className="font-semibold text-gray-900">
+                          {data.tracking_id}
+                        </p>
+                      </div>
+
+                      {/* Status */}
+                      <div>
+                        <p className="text-gray-400">Billing Name</p>
+                        <p className="font-semibold text-gray-900">
+                          {data.billing_name}
+                        </p>
+                      </div>
+
+                      {/* Payment Status */}
+                      <div>
+                        <p className="text-gray-400">Payment Status</p>
+                        <p className="font-semibold text-gray-900">
+                          {data.order_status}
+                        </p>
+                      </div>
+
+                      {/* Payment Method */}
+                      <div>
+                        <p className="text-gray-400">Payment Method</p>
+                        <p className="font-semibold text-gray-900">
+                          {data.payment_mode}
+                        </p>
+                      </div>
+
+                      {/* Address */}
+                      <div>
+                        <p className="text-gray-400">Address</p>
+                        <p className="font-semibold text-gray-900">
+                          {data.billing_address}, {data.billing_city},{" "}
+                          {data.billing_state}, {data.billing_zip},{" "}
+                          {data.billing_country}
+                        </p>
+                      </div>
+
+                      <div>
+                        <p className="text-gray-400">Total Amount Paid</p>
+                        <p className="font-semibold text-gray-900">
+                          {data.currency}, {data.amount}
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="flex justify-center space-x-4 py-5">
+                <Link
+                  href="/bookings"
+                  className="px-6 py-3 bg-blue-600 text-white rounded-lg text-lg font-medium hover:bg-blue-700"
+                >
+                  View Orders
+                </Link>
+                <Link
+                  href="/"
+                  className="px-6 py-3 bg-gray-600 text-white rounded-lg text-lg font-medium hover:bg-gray-700"
+                >
+                  Continue Shopping
+                </Link>
+              </div>
+            </div>
+          </div>
+        </div>
+      ) : (
+        <div className="h-screen flex justify-center items-center bg-gray-100">
+          <div className=" w-2/3 rounded-xl bg-white shadow-sm p-20">
+            <Lottie
+              options={{
+                loop: true,
+                autoplay: true,
+                animationData: Cross,
+                rendererSettings: {
+                  preserveAspectRatio: "xMidYMid slice",
+                },
+              }}
+              height={70}
+              width={70}
+            />
+            <div className="flex justify-center items-center">
+              <div>
+                <div className="mt-12 flex flex-col items-center justify-center">
+                  <h1 className="text-3xl font-medium font-f_2">
+                    Your Order has Failed!
+                  </h1>
+                  <p className="text-gray-500 mt-4">
+                    We are sorry, your order has failed. Please try again.
+                  </p>
+                </div>
+
+                <div className="flex justify-center space-x-4 py-5">
+                  <Link
+                    href="/"
+                    className="px-6 py-3 bg-blue-600 text-white rounded-lg text-lg font-medium hover:bg-blue-700"
+                  >
+                    Continue Shopping
+                  </Link>
+
+                  <Link
+                    href="/bookings"
+                    className="px-6 py-3 bg-gray-600 text-white rounded-lg text-lg font-medium hover:bg-gray-700"
+                  >
+                    Previous Orders
+                  </Link>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
+    </>
   );
 };
 
